3364e1d1edb4e4393b1db7cdfb2869b82f4aa9dc
tatiana <tatiana.galochkina@inserm.fr>
2024-11-26 14:58:38 +0100

    architecture upd

--- model/architecture.py
+++ model/architecture.py
@@ -4,11 +4,20 @@ activation_string_to_function = {
 }
 
 class CNN(nn.Module):
-    def __init__(self, in_channels, hidden_layers=[512], dropout=0.1,
-                 kernel_size=11, activation_function='relu'):
+    def __init__(self, in_channels, hidden_layers=[512], dropout=0.1, kernel_size=11, activation_function='relu'):
         super().__init__()
         if not isinstance(hidden_layers, list):
-…
+            hidden_layers = [hidden_layers]
+
+# Initialize convolutional layers. We use a ModuleList to store them.
+        hidden_layers = [in_channels] + hidden_layers + [1]
+        self.conv_layers = nn.ModuleList()
+        for n_layers, n_layers2 in zip(hidden_layers[:-1], hidden_layers[1:]):
+            self.conv_layers.append(
+                nn.Conv1d(n_layers, n_layers2, kernel_size=kernel_size, padding='same'))
+        self.dropout = nn.Dropout(p=dropout)
+        self.activation_function = activation_string_to_function.get(activation_function, 'relu')
+
     def forward(self, x):
         """Forward pass through the convolutional layers."""
         for conv in self.conv_layers[:-1]:
